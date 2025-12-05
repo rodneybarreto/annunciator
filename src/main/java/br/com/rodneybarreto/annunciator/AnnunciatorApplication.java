0dcf6ac6cf6584b87f29539183d3203e1b5985ac
rodneybarreto <rwbarreto@gmail.com>
2025-12-05 09:44:35 -0300

    first commit

---
+++ src/main/java/br/com/rodneybarreto/annunciator/AnnunciatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodneybarreto.annunciator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnunciatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnunciatorApplication.class, args);
+	}
+
+}
